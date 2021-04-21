5f3342bcda1e31ca88b7f9e8b8f3911563c9d153
ammarsaeed-beejo <ammarsaeed.art@gmail.com>
2021-04-22 02:11:21 +0300

    first commit

---
+++ ssignment9/script.js
@@ -0,0 +1,16 @@
+
+let one=Number(prompt('enter no1',)) || 0;
+
+let two=Number(prompt('enter no2',)) || 0;
+
+if( one > two){
+    alert("The larger number is "+one);
+}
+
+else if (one == two){
+    alert("The number 1&2 are equal");
+}
+
+else{
+    alert("The larger number is "+b )
+}
